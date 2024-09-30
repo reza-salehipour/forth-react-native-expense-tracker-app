0ab46cae17cd0245927830bdcddaaa90c4137c93
Reza Sal <rez.salehipour@gmail.com>
2024-09-30 21:47:55 +0100

    handling dates and improving styles

--- components/ExpensesOutput/ExpensesList.js
+++ components/ExpensesOutput/ExpensesList.js
@@ -1,7 +1,10 @@
-import {FlatList, Text} from "react-native";
+import {FlatList} from "react-native";
+import ExpenseItem from "./ExpenseItem";
 
 function renderExpenseItem(itemData) {
-    return (<Text>{itemData.item.description}</Text>);
+    return (
+        <ExpenseItem {...itemData.item} />
+    );
 }
 
 function ExpenseList({expenses}) {
